9681e4bb68401263d7117d9bc4b87c7c9192bf60
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-11-11 00:21:27 +0700

    done

---
+++ app/src/main/java/com/hacktive8/finalprojects3/MainActivity.java
@@ -0,0 +1,107 @@
+package com.hacktive8.finalprojects3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView resultTv,solutionTv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
+
+        assignId(R.id.button_c);
+//        assignId(R.id.button_open_bracket);
+        assignId(R.id.button_modulo);
+        assignId(R.id.button_divide);
+        assignId(R.id.button_multiply);
+        assignId(R.id.button_plus);
+        assignId(R.id.button_minus);
+        assignId(R.id.button_equals);
+        assignId(R.id.button_0);
+        assignId(R.id.button_1);
+        assignId(R.id.button_2);
+        assignId(R.id.button_3);
+        assignId(R.id.button_4);
+        assignId(R.id.button_5);
+        assignId(R.id.button_6);
+        assignId(R.id.button_7);
+        assignId(R.id.button_8);
+        assignId(R.id.button_9);
+        assignId(R.id.button_ac);
+        assignId(R.id.button_dot);
+
+    }
+
+    void assignId(int id){
+        MaterialButton btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button =(MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if(buttonText.equals("C")){
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+
+        if(buttonText.equals("=")){
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+
+        if(buttonText.equals("X")){
+            if (dataToCalculate.length() < 2){
+                solutionTv.setText("");
+                resultTv.setText("0");
+                return;
+            } else {
+                dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+            }
+        }else{
+            dataToCalculate = dataToCalculate+buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if(!finalResult.equals("Err")){
+            resultTv.setText(finalResult);
+        }
+
+    }
+
+    String getResult(String data){
+        try{
+            Context context  = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Err";
+        }
+    }
+
+}
